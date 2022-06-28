131b27d0489906451c5ad13159377d7c41ee0baf
LucasScotta <lucasmanuelscotta@gmail.com>
2022-06-28 16:34:43 -0300

    'TodoItem'

---
+++ src/TodoItem.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default (props) => {
+    return (
+        <li>
+            <span>C</span>
+            <p>{props.text}</p>
+            <span>X</span>
+        </li>
+    )
+}
